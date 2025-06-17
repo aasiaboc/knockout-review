2d0d7b647f8301602837ac57edfe72e08e8bcd6d
aasiaboc <aasiaboc@csyoutsourcing.com>
2025-06-17 14:32:45 +0800

    Refactor task management and enhance UI components in the to-do list application

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-function AppViewModel(){
-    this.title = ko.observable("TO-DO LIST");
-    this.isImportant = ko.observable(true);
+function Task(name) {
+    this.name = ko.observable(name);
+    this.done = ko.observable(false);
+}
 
+function AppViewModel() {
+    var todo = this;
 
+    todo.title = ko.observable("TO-DO LIST");
+    todo.isImportant = ko.observable(true);
+    todo.text = ko.observable(""); 
+    todo.tasks = ko.observableArray([]);
 
+    todo.addTask = function () {
+        const trimmed = todo.text().trim();
+        if (trimmed) {
+            todo.tasks.push(new Task(trimmed));
+            todo.text("");
+        }
+    };
 
+    todo.toggleTask = function (task) {
+        task.done(!task.done());
+    };
+
+    todo.deleteTask = function (task) {
+        todo.tasks.remove(task);
+    };
 }
 
 ko.applyBindings(new AppViewModel());
