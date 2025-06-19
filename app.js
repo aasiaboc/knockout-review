ba51f350f845897b92899503ac9e94ef81bc6aa5
aasiaboc <aasiaboc@csyoutsourcing.com>
2025-06-19 16:28:27 +0800

    Add task filtering functionality and improve UI layout in to-do list application

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function AppViewModel() {
     todo.isImportant = ko.observable(true);
     todo.text = ko.observable(""); 
     todo.tasks = ko.observableArray([]);
+    todo.filter = ko.observable("all");
 
     todo.addTask = function () {
         const trimmed = todo.text().trim();
@@ -26,6 +27,27 @@ function AppViewModel() {
     todo.deleteTask = function (task) {
         todo.tasks.remove(task);
     };
+
+    todo.editTask = function (task) {
+        const newName = prompt("Edit task name:", task.name());
+        if (newName !== null && newName.trim() !== "") {
+            task.name(newName.trim());
+        }
+    };
+
+    todo.filteredTasks = ko.computed(function () {
+        if (todo.filter() === "all") {
+            return todo.tasks();
+        } else if (todo.filter() === "active") {
+            return ko.utils.arrayFilter(todo.tasks(), function(task) {
+                return !task.done();
+            });
+        } else if (todo.filter() === "completed") {
+            return ko.utils.arrayFilter(todo.tasks(), function(task) {
+                return task.done();
+            });
+        }
+    });
 }
 
 ko.applyBindings(new AppViewModel());
